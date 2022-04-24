ea4bc6d15448fcd76b3210c9d051399b72ab0c86
huzailessani <huzail.isani@gmail.com>
2022-04-24 05:58:57 +0500

    panicking on failure

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,24 @@ use std::io;
 fn main() {
     loop {
         let mut input = String::new();
-        let r = io::stdin().read_line(&mut input);
+        let r = io::stdin()
+            .read_line(&mut input)
+            .expect("please input valid");
         // if let Ok(_) = r {
-        //     if input.trim() == "q" {
-        //         break;
-        //     }
-        //     println!("{}", input);
-        // }
-        match r {
-            Ok(a) => {
-                println!("{}", a);
-                if input.trim() == "q" {
-                    break;
-                }
-                println!("{}", input);
-            }
-            Err(e) => {}
+        if input.trim() == "q" {
+            break;
         }
+        println!("{}", input);
+        // }
+        // match r {
+        //     Ok(a) => {
+        //         println!("{}", a);
+        //         if input.trim() == "q" {
+        //             break;
+        //         }
+        //         println!("{}", input);
+        //     }
+        //     Err(e) => {}
+        // }
     }
 }
