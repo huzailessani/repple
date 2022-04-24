ab7fcad5c887bcb33798e3e3ceff4ed9b04eb2f3
huzailessani <huzail.isani@gmail.com>
2022-04-24 06:27:26 +0500

    changes

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,15 @@ use std::io;
 fn main() {
     loop {
         let mut input = String::new();
-        let r = io::stdin()
-            .read_line(&mut input)
-            .expect("please input valid");
-        // if let Ok(_) = r {
-        if input.trim() == "q" {
-            break;
+        let r = io::stdin().read_line(&mut input);
+        match r {
+            Ok(a) => {
+                if input.trim() == "q" {
+                    break;
+                }
+                println!("{}", input);
+            }
+            Err(e) => {}
         }
-        println!("{}", input);
-        // }
-        // match r {
-        //     Ok(a) => {
-        //         println!("{}", a);
-        //         if input.trim() == "q" {
-        //             break;
-        //         }
-        //         println!("{}", input);
-        //     }
-        //     Err(e) => {}
-        // }
     }
 }
